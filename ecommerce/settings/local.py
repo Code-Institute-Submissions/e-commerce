37309b849f2c2d1de62e6e6cf3b8c344750fdfb7
CristianBurloiu <cristian.burloiu@yahoo.ro>
2018-03-20 11:47:12 +0000

    Third deploy

--- ecommerce/settings/local.py
+++ ecommerce/settings/local.py
@@ -1,11 +1,11 @@
 from .base import * 
 
-DEBUG = True
+# DEBUG = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
