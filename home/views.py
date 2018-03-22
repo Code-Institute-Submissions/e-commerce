c063b1b3256fd28b50ee0f8aae6e7b58b364f501
CristianBurloiu <cristian.burloiu@yahoo.ro>
2018-03-22 16:21:01 +0000

    Added payment option

--- home/views.py
+++ home/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-def get_index(request):
-    return render(request, "home/home.html")
+def home(request):
+    return render(request, "home/index.html")
